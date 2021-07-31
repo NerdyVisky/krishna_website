d83c01a7cd4075d6f66525f9d83d8286186217c4
NerdyVisky <vishvesh106@gmail.com>
2021-07-31 14:52:19 +0530

    added case field, replaced mock images in medpro and extracted js files from html pages

---
+++ copytext.js
@@ -0,0 +1,23 @@
+function copyText(element) {
+    var range, selection, worked;
+
+    if (document.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+
+    try {
+        document.execCommand('copy');
+        alert('Number copied');
+    }
+    catch (err) {
+        alert('unable to copy text');
+    }
+}
